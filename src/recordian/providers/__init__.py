914f4a424d9b2c9d48d72b54b0a15c83010f0a09
zz8011 <zz8011@gmail.com>
2026-02-25 11:35:33 +0800

    refactor: 更新 providers/__init__.py 导出 BaseTextRefiner

--- src/recordian/providers/__init__.py
+++ src/recordian/providers/__init__.py
@@ -1,4 +1,5 @@
 from .base import ASRProvider
+from .base_text_refiner import BaseTextRefiner
 from .cloud_llm_refiner import CloudLLMRefiner
 from .http_cloud import HttpCloudProvider
 from .llamacpp_text_refiner import LlamaCppTextRefiner
@@ -8,6 +9,7 @@ from .streaming_base import StreamingASRProvider
 
 __all__ = [
     "ASRProvider",
+    "BaseTextRefiner",
     "StreamingASRProvider",
     "HttpCloudProvider",
     "QwenASRProvider",
